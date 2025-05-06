af7223ba91ceafc43de6e2e6f318a949fab19315
Jwielings <j.wielings@gmail.com>
2025-05-06 10:35:56 +0200

    Previous tasks completed on a different device.

---
+++ src/main/java/theVariableShop.java
@@ -0,0 +1,31 @@
+public class theVariableShop {
+    public static void main(String[] args) {
+
+        byte smallNumber = 127;
+        short largerNumber = 32767;
+        int evenLargerNumber = 2147483647;
+        long evenLargererNumber = 9223372036854775807L;
+        float decimalNumber = 3.14F;
+        double moreDecimalNumbers = 3.14;
+        boolean trueOrFalse = false;
+        char letter = 'A';
+        String letters = "A string of characters";
+
+        System.out.println(smallNumber);
+        System.out.println(largerNumber);
+        System.out.println(evenLargerNumber);
+        System.out.println(evenLargererNumber);
+        System.out.println(decimalNumber);
+        System.out.println(moreDecimalNumbers);
+        System.out.println(trueOrFalse);
+        System.out.println(letter);
+        System.out.println(letters);
+
+
+
+
+
+
+
+    }
+}
