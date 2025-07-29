aec82cf7891389f911a956fd725f93690d5bfd89
Jwielings <j.wielings@gmail.com>
2025-07-29 14:39:57 +0200

    -

--- src/main/java/Narrative3/RockPaperScissors/Roll.java
+++ src/main/java/Narrative3/RockPaperScissors/Roll.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Roll {
 
     public int makeChoice() {
-        int randomRoll = (int) (Math.random() * 3) + 1; // 1 to 3
+        int randomRoll = (int) (Math.random() * 3) + 1;
         return randomRoll;
     }
 
